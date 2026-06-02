1d2ff52ee6ad971ae9cfe36c7cd9d3501d974316
Falcao <henrique.m.falcao67@gmail.com>
2026-06-02 08:45:09 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 
-import './App.module.css'
+import s from './App.module.css'
+import { useEffect, useState } from 'react'
+import { api } from './constants/api'
+
+import logo from '/logo.svg'
 
 function App() {
+  const [data, setData] = useState([])
+
+
+  useEffect(() => {
+    
+    api.get(`/character`).then((response) => {
+      setData(response.data.results)
+      console.log(response.data.results)
+    }).catch((error) => {
+      CompositionEvent.log("deu Ruim!", error)
+
+    })
+
+  }, [ ])
 
   return (
     <>
-      
+      <h1>aurax</h1>
+      <img className={s.logo} src={logo} alt="Logo" />
+      <main>
+        {data.map((item, index) => {
+          return(
+            <div key={index}>
+              <img src={item.image} alt={item.name} />
+              <h4>name: {item.name}</h4>
+              <p>Species: {item.species}</p>
+              {item.status === "Dead" ? "Status:💀" : item.status === "Alive" ? "Status:❤️" : <p>Status: {item.status}</p>}
+              <p>Origin: {item.origin.name}</p>
+            </div>
+          )
+        })}
+      </main>
     </>
   )
 }
